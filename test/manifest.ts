304088fa0382dd13f20e4303b5a88fc2bb6f40c4
Andrei Iancu <48840313+andreincu@users.noreply.github.com>
2024-12-08 12:53:09 +0200

    removed tests and unnecessary dependencies

--- test/manifest.ts
+++
@@ -1,9 +0,0 @@
-import { defineManifestConfig } from "flugin-manifest-config";
-
-export default defineManifestConfig({
-  name: "Plugin starter",
-  id: "333",
-  main: "./sandbox/index.js",
-  ui: "./ui/index.html",
-  editorType: ["figma", "figjam"],
-});
